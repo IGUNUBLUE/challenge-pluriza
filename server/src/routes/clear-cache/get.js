1a8b874b6930e1a0e4aadcebb6f59cbc4ba0ed8e
Lenin Garizabalo <lcamargo@protonmail.com>
2021-07-09 18:13:52 -0500

    feat(GET /clear-cache): add new endpoint

--- server/src/routes/clear-cache/get.js
+++ server/src/routes/clear-cache/get.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
+const cache = require('../../redis');
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-   res.send('respond with a resource');
+router.get('/', function (req, res) {
+   cache.flushdb(function (error, succeeded) {
+      try {
+         if (error) throw error;
+         res.status(200).json({
+            message: `${succeeded}, the information in the cache was deleted`,
+         });
+      } catch (error) {
+         res.status(500).json({ message: error.message });
+      }
+   });
 });
 
 module.exports = router;

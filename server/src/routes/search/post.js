5570c2b48636b7454c1411a25291dad096da98ed
Lenin Garizabalo <lcamargo@protonmail.com>
2021-07-09 18:15:23 -0500

    fix( POST /search): add validation

--- server/src/routes/search/post.js
+++ server/src/routes/search/post.js
@@ -3,36 +3,42 @@ const axios = require('axios').default;
 const cache = require('../../redis');
 
 router.post('/', async (req, res) => {
-   //users, repositories, issues
    let { typeOfSearch, textToSearch } = req.body;
-   try {
-      cache.get('results', async (error, data) => {
-         if (error) throw error;
-         const type = JSON.parse(data).typeOfSearch;
-         const text = JSON.parse(data).textToSearch;
-         if (data && type === typeOfSearch && text === textToSearch) {
-            res.status(200).json({
-               message: 'Data from redis cache',
-               result: JSON.parse(data),
-            });
-         } else {
-            const { data } = await axios.get(
-               `https://api.github.com/search/${typeOfSearch}?q=${textToSearch}`
-            );
-            const fullResult = {
-               typeOfSearch,
-               textToSearch,
-               github_results: data,
-            };
-            cache.setex('results', 7200, JSON.stringify(fullResult));
-            res.status(200).json({
-               message: 'Data from external API',
-               result: fullResult,
-            });
-         }
+
+   if (typeOfSearch && typeOfSearch) {
+      try {
+         cache.get('results', async (error, data) => {
+            if (error) throw error;
+            const type = JSON.parse(data)?.type;
+            const text = JSON.parse(data)?.text;
+            if (data && type === typeOfSearch && text === textToSearch) {
+               res.status(200).json({
+                  message: 'Data from redis cache',
+                  result: JSON.parse(data),
+               });
+            } else {
+               const { data } = await axios.get(
+                  `https://api.github.com/search/${typeOfSearch}?q=${textToSearch}`
+               );
+               const fullResult = {
+                  type: typeOfSearch,
+                  text: textToSearch,
+                  github_results: data,
+               };
+               cache.setex('results', 7200, JSON.stringify(fullResult));
+               res.status(200).json({
+                  message: 'Data from external API',
+                  result: fullResult,
+               });
+            }
+         });
+      } catch (error) {
+         res.status(500).json({ message: error.message });
+      }
+   } else {
+      res.status(400).json({
+         message: 'Unable to create results, required fields are not complete.',
       });
-   } catch (error) {
-      res.status(500).json({ message: error.message });
    }
 });
 

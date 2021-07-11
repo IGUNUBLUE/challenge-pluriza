c998eeb03a7f182a955a9b092f499f77f723e74c
Lenin Garizabalo <lcamargo@protonmail.com>
2021-07-10 23:59:25 -0500

    add redis heroku enviroment

--- server/src/redis/index.js
+++ server/src/redis/index.js
@@ -1,7 +1,7 @@
 const redis = require('redis');
 const redisPort = process.env.REDIS_PORT || 6379;
 const redisHost = process.env.REDIS_URL || "'127.0.0.1'";
-const client = redis.createClient(redisPort, redisHost);
+const client = redis.createClient(redisHost);
 
 client.on('error', (err) => {
    console.log(err);

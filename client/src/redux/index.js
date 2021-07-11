cb9b766354b3bcb98d3f8220f4e5e142bba99a7f
Lenin Garizabalo <lcamargo@protonmail.com>
2021-07-11 13:26:55 -0500

    redux-persist added

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,10 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-// import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 
 import reducer from './reducer';
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+export const store = createStore(
+   persistedReducer,
+   composeWithDevTools(applyMiddleware(thunk))
+);
+export const persistor = persistStore(store);

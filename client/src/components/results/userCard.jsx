fcc5eb829d35984bbeee7a365c66df1a25edb223
Lenin Garizabalo <lcamargo@protonmail.com>
2021-07-11 19:28:05 -0500

    test add followers and following

--- client/src/components/results/userCard.jsx
+++ client/src/components/results/userCard.jsx
@@ -1,6 +1,24 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 import '../../styles/results/userCard.scss';
 
 const UserCard = ({ data }) => {
+   const [followers, setfollowers] = useState();
+   const [following, setfollowing] = useState();
+
+   useEffect(() => {
+      async function getFoll(){
+         let followers = await axios.get(`${decodeURI(data.followers_url)}`);
+         let following = await axios.get(`${decodeURI(data.following_url)}`);
+         setfollowers(followers.data.length);
+         setfollowing(following.data.length);
+      }
+      getFoll();
+   }, []);
+
    return (
       <div className="card">
          <div className="logo-and-name">
@@ -12,10 +30,15 @@ const UserCard = ({ data }) => {
             <h2>{data.login}</h2>
          </div>
          <div className="information">
-            <div>ID: {data.id}</div>
-            <div>Location:</div>
-            <div>Profile: {data.html_url}</div>
-            <div>Score: {data.score}</div>
+            <div name="ID">🆔: {data.id}</div>
+            <div name="location">📍: Unknown</div>
+            <div name="link_profile">
+               🔗: <a href="data.html_url">{data.html_url}</a>
+            </div>
+            <div>
+               <span name="followers">⬅️:{ followers }</span>
+               <span name="following">➡️:{ following }</span>
+            </div>
          </div>
       </div>
    );

6d2d5c09f3601357c13dac2a073abda83a35834d
Lenin Garizabalo <lcamargo@protonmail.com>
2021-07-11 20:20:54 -0500

    clean styles

--- client/src/components/results/repositorioCard.jsx
+++ client/src/components/results/repositorioCard.jsx
@@ -5,11 +5,15 @@ const RepositorioCard = ({ data }) => {
       <div className="repo-card">
          <div className="repo-name">{data.full_name}</div>
          <div className="repo-information">
-            <div>ID: {data.id}</div>
-            <div>URL: {data.owner.html_url}</div>
-            <div>Score: {data.score}</div>
-            <div>
-               Description: <span className="repo-description">{data.description}</span>
+            <div name="id">🆔: {data.id}</div>
+            <div name="profile_link">
+               🔗: <a href={data.owner.html_url}>look at the repository</a>
+            </div>
+            <div name="starts">✨: {data.stargazers_count}</div>
+            <div name="language">👅: {data.language}</div>
+            <div name="forks">🍴: {data.forks_count}</div>
+            <div name="description">
+               📝: <span className="repo-description">{data.description}</span>
             </div>
          </div>
       </div>
